d322b21a58a776265f10242486a36c443243f844
srzaz <srz2@humboldt.edu>
2021-04-29 19:10:36 +0900

    cleaned up code, created function outside main

--- main.go
+++ main.go
@@ -8,6 +8,33 @@ import (
 
 //Program for converting integer array to
 //array of the english form of the integer
+//determine the digit through modulo and division
+func intToString(num int) string {
+	eng_numbers := [10]string{"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"}
+	engnum := ""
+	hunthous := eng_numbers[num/100000]
+	tenthous := eng_numbers[(num%100000)/10000]
+	thous := eng_numbers[(num%10000)/1000]
+	hun := eng_numbers[(num%1000)/100]
+	ten := eng_numbers[(num%100)/10]
+	one := eng_numbers[(num % 10)]
+
+	//determine size of integer
+	if num >= 100000 {
+		engnum += hunthous + tenthous + thous + hun + ten + one
+	} else if num >= 10000 {
+		engnum += thous + hun + ten + one
+	} else if num >= 1000 {
+		engnum += thous + hun + ten + one
+	} else if num >= 100 {
+		engnum += hun + ten + one
+	} else if num >= 10 {
+		engnum += ten + one
+	} else {
+		engnum += one
+	}
+	return engnum
+}
 
 func main() {
 
@@ -15,7 +42,6 @@ func main() {
 
 	//error handling for input
 	var err error
-	eng_numbers := [10]string{"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"}
 
 	nums := make([]int, len(input))
 
@@ -28,33 +54,13 @@ func main() {
 	//loop through numbers and determine the digit through modulo and division
 	//takes 0 <= n < 1,000,000
 	for i := 0; i < len(nums); i++ {
-		engnum := ""
-		hunthous := eng_numbers[nums[i]/100000]
-		tenthous := eng_numbers[(nums[i]%100000)/10000]
-		thous := eng_numbers[(nums[i]%10000)/1000]
-		hun := eng_numbers[(nums[i]%1000)/100]
-		ten := eng_numbers[(nums[i]%100)/10]
-		one := eng_numbers[(nums[i] % 10)]
+		result := intToString(nums[i])
 
-		if nums[i] >= 100000 {
-			engnum += hunthous + tenthous + thous + hun + ten + one
-		} else if nums[i] >= 10000 {
-			engnum += thous + hun + ten + one
-		} else if nums[i] >= 1000 {
-			engnum += thous + hun + ten + one
-		} else if nums[i] >= 100 {
-			engnum += hun + ten + one
-		} else if nums[i] >= 10 {
-			engnum += ten + one
-		} else {
-			engnum += one
-		}
 		if i != (len(nums) - 1) {
-			fmt.Print(engnum + ", ")
+			fmt.Print(result + ", ")
 		} else {
-			fmt.Println(engnum)
+			fmt.Println(result)
 		}
-
 	}
 
 }
