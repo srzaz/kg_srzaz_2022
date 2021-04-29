ab103cdf01d987a5d3317b67459a64445dc10368
srzaz <srz2@humboldt.edu>
2021-04-29 18:40:48 +0900

    code functionality added

--- main.go
+++ main.go
@@ -3,15 +3,58 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
+
+//Program for converting integer array to
+//array of the english form of the integer
 
 func main() {
 
-	argsWithoutProg := os.Args[1:]
-	numbers := [10]string{"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"}
-	arg := os.Args[3]
+	input := os.Args[1:]
 
-	fmt.Println(numbers)
-	fmt.Println(argsWithoutProg)
-	fmt.Println(arg)
+	//error handling for input
+	var err error
+	eng_numbers := [10]string{"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"}
+
+	nums := make([]int, len(input))
+
+	//convert command line arguments into integer form
+	for i := 0; i < len(input); i++ {
+		if nums[i], err = strconv.Atoi(input[i]); err != nil {
+			panic(err)
+		}
+	}
+	//loop through numbers and determine the digit through modulo and division
+	//takes 0 <= n < 1,000,000
+	for i := 0; i < len(nums); i++ {
+		engnum := ""
+		hunthous := eng_numbers[nums[i]/100000]
+		tenthous := eng_numbers[(nums[i]%100000)/10000]
+		thous := eng_numbers[(nums[i]%10000)/1000]
+		hun := eng_numbers[(nums[i]%1000)/100]
+		ten := eng_numbers[(nums[i]%100)/10]
+		one := eng_numbers[(nums[i] % 10)]
+
+		if nums[i] >= 100000 {
+			engnum += hunthous + tenthous + thous + hun + ten + one
+		} else if nums[i] >= 10000 {
+			engnum += thous + hun + ten + one
+		} else if nums[i] >= 1000 {
+			engnum += thous + hun + ten + one
+		} else if nums[i] >= 100 {
+			engnum += hun + ten + one
+		} else if nums[i] >= 10 {
+			engnum += ten + one
+		} else {
+			engnum += one
+		}
+		if i != (len(nums) - 1) {
+			fmt.Print(engnum + ", ")
+		} else {
+			fmt.Println(engnum)
+		}
+
+	}
+
 }
